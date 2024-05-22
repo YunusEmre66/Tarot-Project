88cd22aa2854435582c11fb066b65740bac5c27f
yunus emre <yunuzemre30@gmail.com>
2024-05-23 01:19:16 +0200

    edit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from "./components/header/Header.jsx"
+import Main from "./components/main/Main.jsx"
+import Navbar from "./components/navbar/Navbar.jsx"
+import "./App.scss"
+function App() {
+  return (
+    <>
+
+       <Navbar/>
+    <Header/>
+    <Main/>
+ 
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss"
+
+const header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">Who are you</span>
+      </div>
+  )
+}
+
+export default header
